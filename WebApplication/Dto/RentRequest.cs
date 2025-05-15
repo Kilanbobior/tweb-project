c861e798193c5a89d42ed8a8e33a1a7d362aae85
Kilanbobior <chilivnicandrei@gmail.com>
2025-05-15 22:42:25 +0300

    Add files via upload

---
+++ WebApplication/Dto/RentRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Dto
+{
+    public class RentRequest
+    {
+        public Guid ProductId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
