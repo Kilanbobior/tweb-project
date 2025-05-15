9457da3142f34b5bf2915de30b4c2ffcdf847629
Kilanbobior <chilivnicandrei@gmail.com>
2025-05-15 22:40:10 +0300

    Update HomeController.cs

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -1,34 +1,62 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication.Dto;
+using WebApplication.Services;
 
 namespace WebApplication.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
-        public ActionResult Index()
+        private readonly IAccountService _userService;
+        private readonly IRentService _rentService;
+
+        public HomeController()
         {
+            _rentService = new RentService();
+            _userService = new AccountService();
+        }
+
+
+
+        public ActionResult Basket()
+        {
+
             return View();
         }
 
-        public ActionResult Autorization()
-        {
-
-            return View();
-        }
-
-        
-
+ 
         public ActionResult index_auto()
         {
             return View();
         }
 
         
-
         
+
+        public ActionResult index()
+        {
+
+            return View();
+        }
+
+        public ActionResult FAQ()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Rent(RentRequest request)
+        {
+            return _rentService.Rent(request);
+        }
+
+
+
+
     }
+
 }
