65201c8093c0a4fa3a268e66e22cf56ea374858b
TarasWeeb <taras.suskii@isa.utm.md>
2025-05-15 22:54:26 +0300

    Add files via upload

---
+++ WebApplication/TWebDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApplication.Models;
+
+namespace WebApplication
+{
+    public class TWebDbContext : DbContext
+    {
+        public TWebDbContext() : base("Server=localhost;port=5432;Database=TWeb;User Id=postgres;Password=141592;")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public  DbSet<Reviews> Reviews { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Настройка связей
+            modelBuilder.Entity<Order>()
+                .HasRequired(o => o.User)
+                .WithMany(u => u.Orders)
+                .HasForeignKey(o => o.UserId);
+
+            modelBuilder.Entity<Order>()
+                .HasRequired(o => o.Product)
+                .WithMany(p => p.Orders)
+                .HasForeignKey(o => o.ProductId);
+        }
+
+
+
+    }
+}
