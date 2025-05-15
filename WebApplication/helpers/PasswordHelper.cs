1e952e7fbd8e70a679e11b5137fbcfe7bbbcf721
d1VooM <draganmitesh@gmail.com>
2025-05-15 22:58:15 +0300

    Add files via upload

---
+++ WebApplication/helpers/PasswordHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TWeb48.Helpers
+{
+    public class PasswordHelper
+    {
+        public static string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(password);
+                var hash = sha256.ComputeHash(bytes);
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+        public static bool VerifyPassword(string password, string hash)
+        {
+            return HashPassword(password) == hash;
+        }
+
+        internal static bool VerifyPassword(string password, object passwordHash)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
