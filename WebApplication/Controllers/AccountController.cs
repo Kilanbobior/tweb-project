43187e58065d97cf92e060f0c0b8631e696ec82a
Kilanbobior <chilivnicandrei@gmail.com>
2025-05-15 21:37:59 +0300

    Update AccountController.cs

--- WebApplication/Controllers/AccountController.cs
+++ WebApplication/Controllers/AccountController.cs
@@ -1,56 +1,101 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using WebApplication.Dto;
+using WebApplication.Models;
+using WebApplication.Services;
 
 namespace WebApplication.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly IAccountService _accountService;
+        private readonly TWebDbContext _webDbContext;
+
+
+        public AccountController()
+        {
+            _accountService = new AccountService();
+            _webDbContext = new TWebDbContext();
+        }
+
+
         // GET: Account
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult edit_prof()
+       
+        public ActionResult Edit_prof()
         {
             return View();
         }
 
+        public ActionResult Edit_prof(User _model)
+        {
+            var model = _accountService.EditProfil(_model);
+            return View();
+        }
 
 
-        // POST: /Account/SingUp
+        public ActionResult Profil(string name)
+        {
+            var user = _webDbContext.Users.FirstOrDefault(x => x.Username == name);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+           
+            return View(user);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
         [HttpPost]
-        public ActionResult SignUp(SignUpRequest request)
+        public ActionResult Login(LoginRequest model)
         {
-
-            //logic will be in lab 5
-
-            return RedirectToAction("Login"); // После регистрации перенаправляем на вход
+            var result = _accountService.Login(model);
+            if (result == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            return new RedirectResult(result);
         }
 
-        // GET: /Account/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
         [HttpPost]
-        public ActionResult Login(LoginRequest request)
+        public ActionResult SignUp(RegisterRequest model)
         {
-            //TODO: lab5
-
-            return View();
-        }
-
-
-        // GET: /Account/Profile
-        public ActionResult profile()
-        {
-            return View();
+            return _accountService.Register(model);
         }
 
         public ActionResult SignUp()
         {
             return View();
         }
+
+
+        public ActionResult History()
+        {
+            var id = Guid.Parse(FormsAuthentication.Decrypt(Request.Cookies[FormsAuthentication.FormsCookieName].Value).UserData.Split(',')[0]);
+            var rents = _webDbContext.Orders.Where(x => x.UserId == id);
+            return View(rents);
+        }
+
+
     }
 }
