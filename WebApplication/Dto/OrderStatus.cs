92bb1b43915e498109fd4ab3e9d4e49e1f2ac1d7
TarasWeeb <taras.suskii@isa.utm.md>
2025-05-15 22:44:18 +0300

    Add files via upload

---
+++ WebApplication/Dto/OrderStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public enum OrderStatus
+    {
+        Pending,
+        Processing,
+        Completed,
+        Cancelled
+    }
+}
