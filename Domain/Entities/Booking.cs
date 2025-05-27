44117db517da5611562e8a5f0632179efdf8237a
TarasWeeb <taras.suskii@isa.utm.md>
2025-05-27 22:04:26 +0300

    Добавил все вью что нцжны были

---
+++ Domain/Entities/Booking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Booking
+    {
+        public Guid Id { get; set; }
+        public Guid VacationBaseId { get; set; }
+        public VacationBase VacationBase { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public BookingStatus Status { get; set; }
+    }
+}
---
+++ Domain/Entities/VacationBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class VacationBase
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string AccommodationType { get; set; } // e.g., "Cabin", "Room", "Suite"
+        public decimal PricePerNight { get; set; }
+        public int Capacity { get; set; }
+        public bool IsAvailable { get; set; }
+        public ICollection<Booking> Bookings { get; set; }
+        public ICollection<Amenity> Amenities { get; set; }
+        public string ImageUrl { get; set; } // Add image for each accommodation option
+    }
+}
